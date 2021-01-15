db46b0c52e4f7d00bcfc27371cf0bfa72dfd020e
Lou CHANCRIN <lchancri@e1r5p14.42.fr>
2021-01-15 15:55:38 +0100

    Cleaning repo

---
+++ C/src/n_puzzle.c
@@ -0,0 +1,20 @@
+#include "../include/n_puzzle.h"
+
+void    main(int ac, char **av)
+{
+    int     a;
+    int     racine_len_av;
+    char    **plateau;
+
+    if (ac != 2)
+    {
+        ft_putstr("Nombre d'arguments invalide")
+    }
+    
+
+    racine_len_av = 3;
+
+    if (!(plateau = (short**)malloc(sizeof(short*) * racine_len_av)))
+        return (0);
+    return (0);
+}
