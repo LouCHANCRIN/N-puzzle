652e09a5b4ab17d0700a132f31abfaa9f49f9ee4
Lou CHANCRIN <lchancri@e1r5p14.42.fr>
2021-01-15 15:32:29 +0100

    Add resources

--- python/n_puzzle.py
+++ python/n_puzzle.py
@@ -9,14 +9,7 @@ class ArgsError(Exception):
 class FormatError(Exception):
     desc = "Invalid number of values"
 
-def heuristique():
-    return 1
-
-def a_star(plateau: list, size: int, empty_pos: list):
-    print(empty_pos)
-    return
-
-def main(plateau: list, size: int, empty_pos: list):
+def print_plat(plateau: list):
     print("Plateau :")
     for i in range(0, size):
         try:
@@ -29,6 +22,15 @@ def main(plateau: list, size: int, empty_pos: list):
             print(plateau[i][0].expected_value, plateau[i][1].expected_value, plateau[i][2].expected_value, plateau[i][3].expected_value)
         except:
             print(plateau[i][0].expected_value, plateau[i][1].expected_value, plateau[i][2].expected_value)
+
+def heuristique():
+    return 1
+
+def a_star(plateau: list, size: int, empty_pos: list):
+    print(empty_pos)
+    return
+
+def main(plateau: list, size: int, empty_pos: list):
     a_star(plateau, size, empty_pos)
 
 if __name__ == "__main__":
@@ -50,6 +52,6 @@ if __name__ == "__main__":
             plateau[int(i / size)].append(Taquin(value, int(i / size), j))
             if int(data[i+j]) == 0:
                 empty_pos = [int(i / size), j]
-    
     plateau = connect_pieces(plateau, size)
+    print_plat(plateau)
     main(plateau, size, empty_pos)
