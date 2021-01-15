002f9ff4ce7ac329b3a86ef55e56d0bbabfdb392
Lou CHANCRIN <lchancri@e1r5p14.42.fr>
2021-01-15 14:46:04 +0100

    Dynamically create plateau using size mentionned in file

--- python/n_puzzle.py
+++ python/n_puzzle.py
@@ -19,10 +19,16 @@ def a_star(plateau: list, size: int, empty_pos: list):
 def main(plateau: list, size: int, empty_pos: list):
     print("Plateau :")
     for i in range(0, size):
-        print(plateau[i][0].current_value, plateau[i][1].current_value, plateau[i][2].current_value)
+        try:
+            print(plateau[i][0].current_value, plateau[i][1].current_value, plateau[i][2].current_value, plateau[i][3].current_value)
+        except:
+            print(plateau[i][0].current_value, plateau[i][1].current_value, plateau[i][2].current_value)
     print("Attendu :")
     for i in range(0, size):
-        print(plateau[i][0].expected_value, plateau[i][1].expected_value, plateau[i][2].expected_value)
+        try:
+            print(plateau[i][0].expected_value, plateau[i][1].expected_value, plateau[i][2].expected_value, plateau[i][3].expected_value)
+        except:
+            print(plateau[i][0].expected_value, plateau[i][1].expected_value, plateau[i][2].expected_value)
     a_star(plateau, size, empty_pos)
 
 if __name__ == "__main__":
@@ -34,7 +40,9 @@ if __name__ == "__main__":
     if len(data) != (size * size + 1):
         raise FormatError
     del data[0]
-    plateau = [[], [], []]
+    plateau = []
+    for i in range(0, size):
+        plateau.append([])
     empty_pos = None
     for i in range(0, size * size, size):
         for j in range(0, size):
@@ -42,5 +50,6 @@ if __name__ == "__main__":
             plateau[int(i / size)].append(Taquin(value, int(i / size), j))
             if int(data[i+j]) == 0:
                 empty_pos = [int(i / size), j]
+    
     plateau = connect_pieces(plateau, size)
     main(plateau, size, empty_pos)
