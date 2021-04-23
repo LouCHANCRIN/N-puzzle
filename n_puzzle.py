2584504b167908f199e4e64ca1e1480a10b89e1f
Lou <louchancrin@hotmail.com>
2021-04-23 18:29:16 +0200

    Clean code

--- n_puzzle.py
+++ n_puzzle.py
@@ -1,6 +1,6 @@
-import sys
 import os
 import argparse
+from pathlib import Path
 
 from read_file import get_data
 from taquin import Taquin, NotSolvableError, UnknownHeuristic
@@ -13,11 +13,10 @@ class ArgsError(Exception):
 def test_errors(path):
     errors = os.listdir(path)
     for dir in errors:
-        current_path = path + '/' + dir
+        current_path = Path(path, dir)
         files = os.listdir(current_path)
         for file in files:
-            current_file = current_path + '/' + file
-            print(current_file)
+            current_file = Path(current_path, file)
             try:
                 size, data = get_data(current_file)
                 taquin = Taquin(size, data)
@@ -25,44 +24,34 @@ def test_errors(path):
                 print("Format error")
 
 def test_unsolvable(path, heuristic):
-    unsolvable_path = path + '/unsolvable'
-    solvable_path = path + '/solvable'
+    unsolvable_path = Path(path, 'unsolvable')
+    solvable_path = Path(path, 'solvable')
 
     solvable = 0
     files = os.listdir(solvable_path)
     len_solvable = len(files)
-    print("SOLVABLE\n\n\n\n")
     if True:
         for file in files:
-            current_file = solvable_path + '/' + file
-            # print(current_file)
+            current_file = Path(solvable_path, file)
             size, data = get_data(current_file)
             try:
                 taquin = Taquin(size, data, heuristic)
-                # return
                 solvable += 1
-                # print("This puzzle is solvable")
             except NotSolvableError:
-                print(current_file)
-                print("UNSOLVABLE ERREUR\n")
+                print(f"{current_file} : unsolvable error")
 
-    print("UNSOLVABLE\n\n\n\n")
     unsolvable = 0
     files = os.listdir(unsolvable_path)
     len_unsolvable = len(files)
     if True:
         for file in files:
-            current_file = unsolvable_path + '/' + file
-            # print(current_file)
+            current_file = Path(unsolvable_path, file)
             size, data = get_data(current_file)
             try:
                 taquin = Taquin(size, data, heuristic)
-                print(current_file)
-                print("SOLVABLE ERREUR\n")
+                print(f"{current_file} : solvable error")
             except NotSolvableError:
                 unsolvable += 1
-                # print("This puzzle is not solvable")
-        print(unsolvable, len(files))
 
     print(solvable, len_solvable)
     print(unsolvable, len_unsolvable)
